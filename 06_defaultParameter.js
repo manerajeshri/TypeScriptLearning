8778ca732441d9c2ffa75f4fc7e7e5c72cf74a3d
manerajeshri <manerajeshri@gmail.com>
2023-01-25 20:44:13 +0530

    defaultParameter
    defaultParameter

---
+++ 06_defaultParameter.js
@@ -0,0 +1,8 @@
+function display(id, name, role) {
+    if (role === void 0) { role = "Guest user"; }
+    console.log('Id :', id);
+    console.log('Id :', name);
+    console.log('Role : ', role);
+}
+display(1, 'aaaru', 'Admin');
+display(2, 'raju');
---
+++ 06_defaultParameter.ts
@@ -0,0 +1,11 @@
+
+function display(id:number , name:string,role:string = "Guest user") {
+console.log('Id :',id);
+console.log('Id :', name);
+console.log('Role : ',role);
+   
+}
+
+display(1,'aaaru','Admin');
+display(2,'raju');
+
