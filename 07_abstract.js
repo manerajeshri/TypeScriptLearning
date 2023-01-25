b18f5ec58a2ecf4da9a5342f50b0112fc36288e5
manerajeshri <manerajeshri@gmail.com>
2023-01-25 20:47:37 +0530

    abstract
    abstract

---
+++ 07_abstract.js
@@ -0,0 +1,66 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.Wheel = function () {
+        return "4 Wheelar";
+    };
+    Car.prototype.CheckAC = function () {
+        return "AC is available ";
+    };
+    Car.prototype.CallFacility = function () {
+        return "Call facility Supported";
+    };
+    return Car;
+}());
+var Hundai = /** @class */ (function (_super) {
+    __extends(Hundai, _super);
+    function Hundai() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Hundai.prototype.Price = function () {
+        return 1000000;
+    };
+    Hundai.prototype.getTotalSeats = function () {
+        return 7;
+    };
+    Hundai.prototype.Color = function () {
+        return "red";
+    }; // creating hundai class from car 
+    return Hundai;
+}(Car));
+var hundai = new Hundai(); // creating new object from
+console.log(hundai.CallFacility());
+var Toyota = /** @class */ (function (_super) {
+    __extends(Toyota, _super);
+    function Toyota() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Toyota.prototype.Price = function () {
+        return 1234567;
+    };
+    Toyota.prototype.getTotalSeats = function () {
+        return 5;
+    };
+    Toyota.prototype.Color = function () {
+        return "white";
+    };
+    return Toyota;
+}(Car));
+var toyota = new Hundai(); // creating new object from
+console.log(hundai.CallFacility());
+console.log(hundai.Price());
