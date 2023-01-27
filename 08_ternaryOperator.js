1ca7166ea9450576cf94c4e2cb8428c4c2dcecbb
manerajeshri <manerajeshri@gmail.com>
2023-01-27 20:35:06 +0530

    ternaryOperator
    ternaryOperator

---
+++ 08_ternaryOperator.js
@@ -0,0 +1,5 @@
+//  08_ternaryOperator.ts
+// ternery operator is euale to if else
+var x = 20;
+var y = 10;
+console.log(x > y ? "x is greater than y" : "y is greater than x");
---
+++ 08_ternaryOperator.ts
@@ -0,0 +1,9 @@
+//  08_ternaryOperator.ts
+
+
+
+// ternery operator is euale to if else
+var x: number =20;
+var y: number =10;
+
+console.log( x>y ? "x is greater than y" : "y is greater than x");
