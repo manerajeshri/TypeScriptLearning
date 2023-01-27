2ea1055df04aa40559926643fd55eb0589537ea0
manerajeshri <manerajeshri@gmail.com>
2023-01-27 20:36:16 +0530

    extendingInterface
    extendingInterface

---
+++ 08_extendingInterface.js
@@ -0,0 +1,10 @@
+var myCar = {
+    make: "Tata",
+    model: "Aulo",
+    year: 2018,
+    color: "Red",
+    numberOfDoors: 4,
+    seats: 5,
+    auto: false
+};
+console.log(myCar);
---
+++ 08_extendingInterface.ts
@@ -0,0 +1,58 @@
+interface IExterior{
+    color : string;
+    numberOfDoors : number;
+}
+
+interface IInterior {
+    seats : number;
+    auto : boolean;
+}
+interface ICar extends IExterior,IInterior{
+    make: string;
+    model : string;
+    year :number;
+}
+
+ var myCar : ICar ={
+    make: "Tata",
+    model: "Aulo",
+    year: 2018,
+    color: "Red",
+    numberOfDoors: 4,
+    seats: 5,
+    auto: false
+}
+
+console.log(myCar);
+
+
+//===============================  sir
+// interface IExterior {
+//     color: string;
+//     numOfDoors: number;
+// }
+
+// interface IInterior {
+//     seats: number;
+//     auto: boolean;
+// }
+
+// interface IAC {
+//     ac: boolean;
+
+// }
+// interface ICar extends IExterior, IInterior {
+//     make: string;
+//     model: string;
+//     year: number
+// }
+
+// var myCar : ICar = {
+//     make: "Tata",
+//     model: "Altroz",
+//     year: 2018,
+//     color: "BLACK",
+//     numOfDoors: 4,
+//     seats: 5,
+//     auto: false
+// }
