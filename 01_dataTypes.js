cce37d8e1e7df284e510f498492a5eb67bf0c9fc
manerajeshri <manerajeshri@gmail.com>
2023-02-25 23:52:16 +0530

    datatypes
    datatypes

---
+++ 01_dataTypes.js
@@ -0,0 +1,11 @@
+var s1 = " You are creater of your destiny";
+var s1 = "Powerfull people come from powerful places ";
+var s3 = ' ';
+var b1 = true;
+var b2 = false;
+var b3;
+function test() {
+     b3 = false;
+    console.log("line no 16", b3); // false
+}
+console.log("line no 20", test());
---
+++ 01_dataTypes.ts
@@ -0,0 +1,34 @@
+
+var s1: string =" You are creater of your destiny";
+var s1: string =`Powerfull people come from powerful places `;
+var s3: string = ' ' ;
+
+
+var b1 : boolean = true;
+var b2 : boolean = false;
+var b3 : boolean ;
+
+function test(): boolean {
+    return b3 = false ;
+    console.log(`line no 13`,b3); // false
+    
+}
+
+console.log(`line no 17`,test());
+
+
+// any data types
+
+let value : any;
+
+value = true;
+value = 42;
+value = `test`;
+value = [ ];
+
+
+// unknown
+// the unknown type
+let val : unknown;
+
+let val1: unknown = val;
