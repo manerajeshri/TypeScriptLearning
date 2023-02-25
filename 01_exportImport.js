a815687137f6d7de885ee5ba97cccc9a0949e5b8
manerajeshri <manerajeshri@gmail.com>
2023-02-25 23:52:36 +0530

    Export Import
    Export Import

---
+++ 01_exportImport.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var utils_1 = require("./utils");
+console.log((0, utils_1.isPalindromessss)('madam'));
+console.log((0, utils_1.isPalindromessss)('madan'));
---
+++ 01_exportImport.ts
@@ -0,0 +1,3 @@
+import {isPalindromessss} from './utils'
+console.log(isPalindromessss('madam'));
+console.log(isPalindromessss('madan'));
