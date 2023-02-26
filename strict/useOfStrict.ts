d521951fa9fe748759afe6c9e6396b64cced3830
manerajeshri <manerajeshri@gmail.com>
2023-02-26 14:02:27 +0530

    strict
    strict

---
+++ strict/useOfStrict.ts
@@ -0,0 +1,16 @@
+// npx tsc --init
+// JSON : JavaScript Object Notation 
+
+// API == 
+
+function add(first: string, second: string){
+    return second + first;
+}
+
+add(1 , 2);
+
+add('hello', 'word'); 
+
+let empId : number   = 101;
+
+empId = null;
