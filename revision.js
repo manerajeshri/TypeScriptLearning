58ce70bb28fc551604060f924b9cbf111628f8b8
manerajeshri <manerajeshri@gmail.com>
2023-04-16 17:32:59 +0530

    revision - alert(); confirm() prompt()
    revision - alert(); confirm() prompt()

--- revision.js
+++ revision.js
@@ -1,15 +1,12 @@
 // code done on typescript playground
-
 // var x : number;
 // x=123
 // var y : string ="raj"
-
 // function addnum(num1: number, num2: number) :number{
 // return num1 + num2
 // }
 // // let result = addnum(1,4)
 // console.log(addnum(1,4))
-
 // class Student {
 // public firstname: string;
 // private lastname: string;
@@ -18,42 +15,24 @@
 // this.lastname = lastname
 //     }
 // }
-
 // var student1 =new Student("Raj", "Mane") // creating object student1 
 // // while using new keyword we are assigning memory
 // console.log(student1)
-
-
-
 // module import
-
 // import {isPalindromessss} from "./utils";
 // console.log(isPalindromessss("madam"));
-
 // alert(); confirm() prompt() are inbuild methods
 // alert();
 // alert("alert msg")
-
 // confirm()
 // confirm("confirm msg ?? ")
-
 // to take user input we use prompt()
-
 // var enteredName= prompt('please enter your name')
 // console.log(`your name : `,enteredName);
 // alert( "name is " + enteredName )
-
-var num1 = Number(prompt(`enter num1 `))
-var num2 = Number(prompt(`enter num2 `))
-console.log(`addition is`, num1 + num2);
-alert(num1 + num2)
-
-console.log(`parseInt("32px") : `, parseInt("32px")); // 32
-console.log(`Number("32dny")`, Number("32dny")); // NaN
-
-
-
-
-
-
-
+var num1 = Number(prompt("enter num1 "));
+var num2 = Number(prompt("enter num2 "));
+console.log("addition is", num1 + num2);
+alert(num1 + num2);
+console.log("parseInt(\"32px\") : ", parseInt("32px"));
+console.log("Number(\"32dny\")", Number("32dny"));
